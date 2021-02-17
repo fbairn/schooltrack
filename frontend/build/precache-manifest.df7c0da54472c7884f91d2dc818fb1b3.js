0cd57e95ed103dd854509fb2064f74de6d1aeb18
Fredrick Bairn <fred@17grapes.com>
2021-02-17 10:25:00 -0700

    Added build

---
+++ frontend/build/precache-manifest.df7c0da54472c7884f91d2dc818fb1b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f94a226b2237961e541ce7870ac96090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30afa160d3928c687139",
+    "url": "/static/css/2.1e966a76.chunk.css"
+  },
+  {
+    "revision": "d0af4b7d27e6b7e72bef",
+    "url": "/static/css/main.a36b3359.chunk.css"
+  },
+  {
+    "revision": "30afa160d3928c687139",
+    "url": "/static/js/2.65a43002.chunk.js"
+  },
+  {
+    "revision": "d0af4b7d27e6b7e72bef",
+    "url": "/static/js/main.cdfc1738.chunk.js"
+  },
+  {
+    "revision": "610b963a08a7f7d872c9",
+    "url": "/static/js/runtime~main.066cd76b.js"
+  }
+]);
